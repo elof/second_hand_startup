e93061f2aed80cf6a4fc269317f10617f56c1be7
Rick <RickyMinno@commander.local>
2013-08-03 14:34:29 -0700

    sign in & sign out views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Project2::Application.routes.draw do
 
+resources :users
+resources :sessions, only: [:new, :create, :destroy]
 root :to => 'events#index'
+
+get '/signup' => 'users#new'
+get '/signin' => 'sessions#new'
+get '/signout' => 'sessions#destroy'
 end
