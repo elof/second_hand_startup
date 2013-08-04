40b855187fbba6779dfe6426876b8105889b97e2
karlmageddon <karl.puzon@gmail.com>
2013-08-03 22:34:59 -0700

    Remove duplicate instance of jQuery.js

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,4 @@
+// This is the file that contains all the map-related javascript
+
+var map = L.mapbox.map('map', 'karlmageddon.map-y5aw14d6')
+    .setView([37.76732233927271, -122.41430282592772], 12);
